a693f1e2ea03c68383621eb50a7d42f7226ac3f5
kenken <kenken.gmail.com>
2020-12-25 17:41:27 +0900

    pod installとgitignoreの作成まで実施

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 中村憲佑 on 2020/12/25.
+//  Copyright © 2020 kensuke.nakamura. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextField: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
